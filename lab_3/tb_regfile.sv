3511717eccca4067c72508addf9c1d2e1c7cf610
kenny2817 <kenny2817.qm@gmail.com>
2026-03-28 15:57:38 +0100

    feat(scoreboard)

--- lab_3/tb_regfile.sv
+++ lab_3/tb_regfile.sv
@@ -12,9 +12,7 @@ endpackage
 
 import constants::*;
 
-interface regfile_if (
-    input logic clk
-);
+interface regfile_if (input logic clk);
 
     logic                      rst_n, wr_en, err;
     logic [ADDR_WIDTH - 1 : 0] wr_addr, rd_addr1, rd_addr2;
@@ -28,6 +26,9 @@ interface regfile_if (
 endinterface
 
 class regfile_mail;
+
+    // RESET
+    bit                           rst_n;
 
     // INPUTS
     rand bit                      wr_en; 
@@ -146,11 +147,14 @@ endclass
 class regfile_monitor;
 
     virtual interface regfile_if.dut regfile_If;
+    mailbox #(regfile_mail) mon_scb_mbx;
     mailbox #(regfile_mail) mon_chk_mbx;
 
     function new(virtual interface regfile_if.dut regfile_If,
+                 mailbox #(regfile_mail) mon_scb_mbx,
                  mailbox #(regfile_mail) mon_chk_mbx);
-        this.regfile_If = regfile_If;       
+        this.regfile_If = regfile_If;
+        this.mon_scb_mbx = mon_scb_mbx;
         this.mon_chk_mbx = mon_chk_mbx;
     endfunction
 
@@ -159,8 +163,10 @@ class regfile_monitor;
         regfile_mail mail;
 
         forever begin
-            @(regfile_If.clk);
+            @(posedge regfile_If.clk);
 
+            mail = new();
+            mail.rst_n    = regfile_If.rst_n;
             mail.wr_en    = regfile_If.wr_en;
             mail.wr_addr  = regfile_If.wr_addr;
             mail.wr_data  = regfile_If.wr_data;
@@ -170,6 +176,7 @@ class regfile_monitor;
             mail.rd_data2 = regfile_If.rd_data2;
             mail.err      = regfile_If.err;
             
+            mon_scb_mbx.put(mail);
             mon_chk_mbx.put(mail);
 
             $display("Time: %6t | rst: %b | en: %b | wr_addr: %2d | wr_data: %0d | err: %b | rd_addr1: %2d | rd_addr2: %2d | rd_data1: %0d | rd_data2: %0d |",
@@ -183,10 +190,18 @@ endclass
 class regfile_scoreboard;
 
     mailbox #(regfile_mail) mon_scb_mbx;
-    logic []
+    logic [DATA_WIDTH - 1 : 0] golden_model_data[NUM_REG];
+    int err_count = 0;
+
+    task reset();
+        for (int i = 0; i < NUM_REG; i++) begin
+            golden_model_data[i] <= '0;
+        end
+    endtask 
 
     function new(mailbox #(regfile_mail) mon_scb_mbx);
         this.mon_scb_mbx = mon_scb_mbx;
+        this.reset();
     endfunction
 
     task run();
@@ -197,12 +212,38 @@ class regfile_scoreboard;
 
             mon_scb_mbx.get(mail);
 
+            if (!mail.err) begin
 
+                // check read data 1
+                assert(golden_model_data[mail.rd_addr1] == mail.rd_data1)
+                    else begin
+                        $error("Read 1 failed: %0h != %0h",
+                                golden_model_data[mail.rd_addr1],
+                                mail.rd_data1);
+                        err_count <= err_count + 1;
+                    end
 
-            
+                // check read data 2
+                assert(golden_model_data[mail.rd_addr2] == mail.rd_data2)
+                    else begin
+                        $error("Read 2 failed: %0h != %0h",
+                                golden_model_data[mail.rd_addr2],
+                                mail.rd_data2);
+                        err_count <= err_count + 1;
+                    end
+
+                // update golden model
+                golden_model_data[mail.wr_addr] <= mail.wr_data;
+            end
+
+            if (!mail.rst_n) reset();
         end
 
     endtask
+
+    function print_error_count();
+        $display("Scoreboard error count: %d", err_count);
+    endfunction
 
 endclass
 
@@ -215,11 +256,13 @@ module tb_regfile;
     regfile_if regfile_If(clk);
 
     mailbox #(regfile_mail) gen_drv_mbx;
+    mailbox #(regfile_mail) mon_scb_mbx;
     mailbox #(regfile_mail) mon_chk_mbx;
 
-    regfile_generator gen;
-    regfile_driver drv;
-    regfile_monitor mon;
+    regfile_generator   gen;
+    regfile_driver      drv;
+    regfile_monitor     mon;
+    regfile_scoreboard  scb;
 
     `DUT_NAME dut (
         .clk      (clk),
@@ -237,25 +280,28 @@ module tb_regfile;
     initial begin
 
         gen_drv_mbx = new(1);
+        mon_scb_mbx = new(); // unbounded
         mon_chk_mbx = new(); // unbounded
 
         gen = new(gen_drv_mbx, 10, 1234);
         drv = new(regfile_If, gen_drv_mbx);
-        mon = new(regfile_If);
+        mon = new(regfile_If, mon_scb_mbx, mon_chk_mbx);
+        scb = new(mon_scb_mbx);
+
+        drv.init_dut();
 
         fork
             drv.run();
             mon.run();
+            scb.run();
         join_none
-
-        drv.init_dut();
         
         gen.run();
 
         repeat(5) @(posedge clk);
 
+        scb.print_error_count();
         $finish();
-        
     end
 
 endmodule
