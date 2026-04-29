9fdf4f037c2ee9827338e52c4d9791640d45cc34
kenny2817 <kenny2817.qm@gmail.com>
2026-04-29 14:03:26 +0200

    feat(overview)

--- lab_5/fifo/tb_fifo.sv
+++ lab_5/fifo/tb_fifo.sv
@@ -36,34 +36,33 @@ module tb_fifo;
     rd_en = 0;
     wr_data = '0;
     #20;
-    rst_n = 1; @(posedge clk);
-    rst_n = 0; @(posedge clk);
-    rst_n = 1; @(posedge clk);
+    rst_n = 1;
   
     // Wait until reset is released on a clock edge
     @(posedge clk);
     wait (rst_n);
 
+/*
     // Stimulus
-    @(posedge clk);
-    wr_en   = 1;
-    rd_en   = 0;
-    repeat (17) begin
-      wr_data = 8'hAA;
+    repeat (5) begin
       @(posedge clk);
+      wr_en   = 1;
+      rd_en   = 0;
+      wr_data = $urandom_range(0,255);
     end
 
-    wr_en = 0;
-    rd_en = 0;
-    @(posedge clk);
-    
-    wr_en = 1;
-    rd_en = 1;
-    @(posedge clk);
-    
-    wr_en = 0; rd_en = 1; repeat (16) @(posedge clk);
-    wr_en = 1; rd_en = 0; wr_data = 8'h00; repeat (16) @(posedge clk);
-    wr_en = 0; rd_en = 1; repeat (16) @(posedge clk);
+    repeat (10) begin
+      @(posedge clk);
+      wr_en = 0;
+      rd_en = 0;
+    end
+
+    repeat (2) begin
+      @(posedge clk);
+      wr_en = 0;
+      rd_en = 1;
+    end
+*/
 
     // ADD ADDITIONAL STIMULUS AS NEEDED HERE
 
