bee8c27f540a2be00f09b55dd3ea2bd0b8ca0bdc
kenny2817 <kenny2817.qm@gmail.com>
2026-05-29 11:37:52 +0200

    fix(proj) tb

--- project/gcd/tb_uvm_gcd.sv
+++ project/gcd/tb_uvm_gcd.sv
@@ -642,12 +642,12 @@ package gcd_pkg;
         endfunction
 
         task body();
-            uvm_config_db#(int unsigned)::set(this, "*scb*", "expected_trans", predefined_a.size()); 
-            
             gcd_det_in_seq  in_seq;
             gcd_det_out_seq out_seq;
             in_seq  = gcd_det_in_seq::type_id::create("in_seq");
             out_seq = gcd_det_out_seq::type_id::create("out_seq");
+
+            uvm_config_db#(int unsigned)::set(this, "*scb*", "expected_trans", predefined_a.size()); 
 
             in_seq.predefined_a     = predefined_a;
             in_seq.predefined_b     = predefined_b;
@@ -735,12 +735,12 @@ package gcd_pkg;
         endfunction
 
         task body();
-            uvm_config_db#(int unsigned)::set(this, "*scb*", "expected_trans", num_trans); 
-
             gcd_rnd_in_seq  in_seq;
             gcd_rnd_out_seq out_seq;
             in_seq  = gcd_rnd_in_seq::type_id::create("in_seq");
             out_seq = gcd_rnd_out_seq::type_id::create("out_seq");
+            
+            uvm_config_db#(int unsigned)::set(this, "*scb*", "expected_trans", num_trans); 
 
             in_seq.num_trans = num_trans;
             in_seq.min_a     = min_a;
