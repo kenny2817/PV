7f4931bae180ace56edd7d69617efb882e062c19
kenny2817 <kenny2817.qm@gmail.com>
2026-05-31 11:14:39 +0200

    fix(proj) coverage

--- project/gcd/tb_uvm_gcd.sv
+++ project/gcd/tb_uvm_gcd.sv
@@ -54,28 +54,28 @@ interface gcd_interface (
     // inputs
     chk_in_valid_no_drop: assert property(p_valid_no_drop(in_valid, in_ready))
         else `uvm_error("CHK", "Input Protocol Violation: in_valid dropped without in_ready!")
-    cov_in_valid_no_drop: cover property(p_valid_no_drop(in_valid, in_ready))
+    cov_in_valid_no_drop: cover property(p_valid_no_drop(in_valid, in_ready));
         
     chk_in_data_stable_a: assert property(p_data_stable(in_valid, in_ready, a_in))
         else `uvm_error("CHK", "Input Protocol Violation: a_in changed while stalled!")
-    cov_in_data_stable_a: cover property(p_data_stable(in_valid, in_ready, a_in))
+    cov_in_data_stable_a: cover property(p_data_stable(in_valid, in_ready, a_in));
         
     chk_in_data_stable_b: assert property(p_data_stable(in_valid, in_ready, b_in))
         else `uvm_error("CHK", "Input Protocol Violation: b_in changed while stalled!")
-    cov_in_data_stable_b: cover property(p_data_stable(in_valid, in_ready, b_in))
+    cov_in_data_stable_b: cover property(p_data_stable(in_valid, in_ready, b_in));
 
-    cov_in_handshake: cover property(p_handshake(in_valid, in_ready))
+    cov_in_handshake: cover property(p_handshake(in_valid, in_ready));
 
     // outputs
     chk_out_valid_no_drop: assert property(p_valid_no_drop(out_valid, out_ready))
         else `uvm_error("CHK", "Output Protocol Violation: out_valid dropped without out_ready!")
-    cov_out_valid_no_drop: cover property(p_valid_no_drop(out_valid, out_ready))
+    cov_out_valid_no_drop: cover property(p_valid_no_drop(out_valid, out_ready));
         
     chk_out_data_stable: assert property(p_data_stable(out_valid, out_ready, gcd_out))
         else `uvm_error("CHK", "Output Protocol Violation: gcd_out changed while stalled!")
-    cov_out_data_stable: cover property(p_data_stable(out_valid, out_ready, gcd_out))
+    cov_out_data_stable: cover property(p_data_stable(out_valid, out_ready, gcd_out));
 
-    cov_out_handshake: cover property(p_handshake(out_valid, out_ready))
+    cov_out_handshake: cover property(p_handshake(out_valid, out_ready));
     
     // known control
     chk_in_valid_known:  assert property(p_control_known(in_valid));
@@ -538,6 +538,7 @@ package gcd_pkg;
         endgroup
 
         virtual function void write_in(gcd_input_transaction t);
+            `uvm_info("COV", t.convert2string(), UVM_MEDIUM)
             cov_in.sample(t.a, t.b);
         endfunction
         
@@ -552,6 +553,7 @@ package gcd_pkg;
         endgroup
 
         virtual function void write_out(gcd_output_transaction t);
+            `uvm_info("COV", t.convert2string(), UVM_MEDIUM)
             cov_out.sample(t.gcd);
         endfunction
 
